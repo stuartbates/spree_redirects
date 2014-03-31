9dc51c5dde9f70d3bf6b4e46cb109fcca213004d
Stuart Bates <stuart@builtbyclick.com>
2014-03-31 16:03:25 +0100

    Altered deface override to hook into the menu on the configurations admin page

--- app/overrides/spree_redirects.rb
+++ app/overrides/spree_redirects.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path  => "spree/admin/configurations/index",
-                     :name          => "spree_redirect_config_opt",
-                     :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :partial       => "spree/admin/shared/redirect_config",
+Deface::Override.new(:virtual_path  => 'spree/admin/shared/configuration_menu',
+                     :name          => 'spree_redirect_config_opt',
+                     :insert_bottom => '[data-hook="admin_configurations_sidebar_menu"]',
+                     :partial       => 'spree/admin/shared/redirect_config',
                      :disabled      => false)
